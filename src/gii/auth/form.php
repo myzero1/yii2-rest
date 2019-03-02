b81d38e8ffa9711ff0240469e16aac24bd9f43dd
myzero1 <myzero1@sina.com>
2019-03-02 11:11:37 +0800

    holdon

--- src/gii/auth/form.php
+++ src/gii/auth/form.php
@@ -3,9 +3,9 @@
 /* @var $form yii\widgets\ActiveForm */
 /* @var $generator yii\gii\generators\crud\Generator */
 
-echo $form->field($generator, 'modelClass');
-echo $form->field($generator, 'searchModelClass');
+// echo $form->field($generator, 'modelClass');
+// echo $form->field($generator, 'searchModelClass');
 echo $form->field($generator, 'controllerClass');
 echo $form->field($generator, 'baseControllerClass');
 // echo $form->field($generator, 'apiConfig');
-echo $form->field($generator, 'tableName')->textInput(['table_prefix' => $generator->getTablePrefix()]);
+// echo $form->field($generator, 'tableName')->textInput(['table_prefix' => $generator->getTablePrefix()]);
